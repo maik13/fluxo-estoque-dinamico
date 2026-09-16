f75fa13970c80eda87765518526e7ece6faec19f
Francis Maikom <maikom708@gmail.com>
2026-09-16 15:24:06 -0300

    fix(gerencial): remover rota separada do painel ao vivo

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Auth from "./pages/Auth";
 import ResetPassword from "./pages/ResetPassword";
-import PainelProducaoAoVivo from "./pages/PainelProducaoAoVivo";
 import { AuthProvider } from "@/contexts/AuthContext";
 
 const queryClient = new QueryClient();
@@ -23,7 +22,6 @@ const App = () => (
             <Route path="/" element={<Index />} />
             <Route path="/auth" element={<Auth />} />
             <Route path="/reset-password" element={<ResetPassword />} />
-            <Route path="/producao/ao-vivo" element={<PainelProducaoAoVivo />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
