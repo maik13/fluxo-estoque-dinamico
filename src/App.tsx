237635aebd4425e9c17e1ad6498495bb51c8df20
Francis Maikom <maikom708@gmail.com>
2026-09-16 14:59:29 -0300

    feat(producao): adicionar rota do painel ao vivo

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Auth from "./pages/Auth";
 import ResetPassword from "./pages/ResetPassword";
+import PainelProducaoAoVivo from "./pages/PainelProducaoAoVivo";
 import { AuthProvider } from "@/contexts/AuthContext";
 
 const queryClient = new QueryClient();
@@ -22,6 +23,7 @@ const App = () => (
             <Route path="/" element={<Index />} />
             <Route path="/auth" element={<Auth />} />
             <Route path="/reset-password" element={<ResetPassword />} />
+            <Route path="/producao/ao-vivo" element={<PainelProducaoAoVivo />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
